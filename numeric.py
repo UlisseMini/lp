b2504b71a047779a89821ca4e3f283bba01f9a56
Ulisse Mini <ulisse.mini@gmail.com>
2021-06-24 14:57:19 -0400

    Add hessian, refactor numerics

--- numeric.py
+++ numeric.py
@@ -1,17 +1,58 @@
-from typing import Callable
+from typing import Callable, List
 import numpy as np
 np.set_printoptions(suppress=True, precision=6)
 
-def gradient(f: Callable, x: np.array, d = 1e-5):
-    "Compute the gradient of f at x using central differences"
+def is_floating_point(x: np.ndarray) -> bool:
+    "Predicate to avoid debugging hell when passed np.int64"
+    return x.dtype in (np.float16, np.float32, np.float64)
+
+def derivative(f: Callable, x: float, d = 1e-5):
+    "Derivative of f at x"
+    return (f(x + d) - f(x - d))/(d*2)
+
+
+def derivative_wr(f: Callable, i: int, x: np.ndarray, d = 1e-5):
+    "Derivative of f at x w/r to x[i]"
+    assert is_floating_point(x)
+    def _f(x_i):
+        bak = x[i]; x[i] = x_i
+        v = f(x)
+        x[i] = bak
+        return v
+
+    return derivative(_f, x[i], d=d)
+
+def derivative_wr_many(f: Callable, variables: List[int], x: np.ndarray, d=1e-5):
+    "Derivative of f at x w/r to many variables (higher order)"
+    assert is_floating_point(x)
+    if len(variables) == 0: return f(x)
+    if len(variables) == 1: return derivative_wr(f, variables[0], x, d=d)
+
+    deriv = lambda x: derivative_wr(f, variables[0], x, d=d)
+    return derivative_wr_many(deriv, variables[1:], x)
+
+
+def gradient(f: Callable, x: np.ndarray, d = 1e-5):
+    "Compute the gradient of f at x"
+    assert is_floating_point(x)
     g = np.zeros(len(x))
 
     for i in range(len(x)):
-        dx = np.zeros(len(x))
-        dx[i] = d
-
-        g[i] = (f(x + dx) - f(x - dx)) / (d*2)
+        g[i] = derivative_wr(f, i, x, d=d)
 
     return g
 
+
+def hessian(f: Callable, x: np.array, d = 1e-5):
+    "Compute the hessian of f at x"
+    assert is_floating_point(x)
+
+    n = len(x)
+
+    h = np.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            h[i][j] = derivative_wr_many(f, [i, j], x, d=d)
+
+    return h
 
--- test_numeric.py
+++ test_numeric.py
@@ -2,27 +2,56 @@ import unittest
 import numpy as np
 import numeric
 
-def f(x):
-    return x[0]**2 + 2*x[1]
+def f(v):
+    x, y = v
+    return y * x**2 + 2*y**3 * x
 
-def nabla_f(x):
-    return np.array([2*x[0], 2])
+def gradient_f(v):
+    x, y = v
+    return np.array([y*2*x + 2*y**3, x**2 + 6*y**2 * x])
 
+def hessian_f(v):
+    x, y = v
+    return np.array([
+        [2*y, 2*x + 6*y**2],
+        [2*x + 6*y**2, 12*x*y],
+    ])
 
 class TestNumerics(unittest.TestCase):
-    def test_gradient(self):
+    def test_numerics(self):
         xs = [
             [1,1], [3,1], [5,5], [-1,3],
         ]
+        atol = 1e-3
         for x in xs:
-            x = np.array(x)
-            estimate = numeric.gradient(f, x)
-            real = nabla_f(x)
-            print('real', real)
-            print('estimate', estimate)
-            self.assertTrue(
-                np.allclose(estimate, real, atol=1e-3),
-                f'want {real} got {estimate}'
+            # fp32 doesn't work well, i don't know numerical analysis send help pls
+            x = np.array(x).astype(np.float64)
+            with self.subTest(f"x = {x}"):
+                estimate = numeric.gradient(f, x)
+                real = gradient_f(x)
+                self.assertTrue(
+                    np.allclose(estimate, real, atol=atol),
+                    f'want {real} got {estimate}'
+                )
+
+                estimate_h = numeric.hessian(f, x)
+                real_h = hessian_f(x)
+                self.assertTrue(
+                    np.allclose(estimate_h, real_h, atol=atol),
+                    f'want {real_h} got {estimate_h}'
+                )
+
+    def test_derivative(self):
+        def _f(x):
+            return 3*x**2 + 2
+
+        def _deriv(x):
+            return 6*x
+
+        xs = [1,2,3]
+        for x in xs:
+            x = np.array(x).astype(np.float32)
+            self.assertAlmostEqual(
+                _deriv(x),
+                numeric.derivative(_f, x)
             )
-
-
